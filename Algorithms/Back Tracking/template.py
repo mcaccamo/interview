f412291daa30de0261c1187a739ca3ae8c7222ab
mcaccamo <michaelbcaccamo@gmail.com>
2022-06-06 21:24:29 -0500

    Update template.py

--- Algorithms/Back Tracking/template.py
+++ Algorithms/Back Tracking/template.py
@@ -1,3 +1,5 @@
+#https://leetcode.com/problems/subsets/discuss/27281/A-general-approach-to-backtracking-questions-in-Java-(Subsets-Permutations-Combination-Sum-Palindrome-Partitioning)
+
 def backtrack(candidate):
     if find_solution(candidate):
         output(candidate)
